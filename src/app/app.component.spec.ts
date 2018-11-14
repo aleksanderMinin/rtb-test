8b714f87164f2d869f0df9d1e4f59ce4be94b29d
Minin Aleksandr <aminin@ics.perm.ru>
2018-11-14 09:11:20 +0500

    13/11/18

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,7 @@ import { AppComponent } from './app.component';
 import { FormsModule }   from '@angular/forms';
 import { BrowserModule, By } from '@angular/platform-browser';
 import { BLOCKS } from './default-blocks';
+import { Utils } from './utils';
 
 import { EmailsEditorComponent } from './emails-editor/emails-editor.component';
 import { EmailBlockComponent } from './emails-editor/email-block/email-block.component';
@@ -30,8 +31,30 @@ describe('AppComponent', () => {
 
   it('should renders', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.componentRef.instance.getEmailsCount();
     fixture.detectChanges();
+    fixture.componentRef.instance.addEmail();
+    fixture.detectChanges();
+    fixture.componentRef.instance.getEmailsCount();
+    fixture.componentRef.instance.addEmail();
+    fixture.detectChanges();
+    fixture.componentRef.instance.getEmailsCount();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('emails-editor')).toBeTruthy();
+  });
+
+  it('test util randomNumber', () => {
+    let randomNumber: number;
+    let greater1Less20: boolean = true;
+    for (let i = 0; i < 20; i++) {
+      randomNumber = Utils.randomNumber(1, 20);
+      greater1Less20 = randomNumber >= 1 && randomNumber <= 20 && greater1Less20;
+    }
+
+    expect(greater1Less20).toBeTruthy();
+  });
+
+  it('test util randomString', () => {
+    expect(Utils.randomString(10).length === 10).toBeTruthy();
   });
 });
--- src/app/emails-editor/emails-editor.component.spec.ts
+++ src/app/emails-editor/emails-editor.component.spec.ts
@@ -3,6 +3,7 @@ import { FormsModule }   from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { EmailsEditorComponent } from './emails-editor.component';
 import { EmailBlockComponent } from './email-block/email-block.component';
+import * as $ from 'jquery';
 
 describe('EmailsEditorComponent', () => {
   let component: EmailsEditorComponent;
@@ -32,9 +33,36 @@ describe('EmailsEditorComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should create invalid block', () => {
-    fixture;
-    component.addBlock('123');
-    expect(component).toBeTruthy();
+  it('should add blocks', () => {
+    fixture = TestBed.createComponent(EmailsEditorComponent);
+    fixture.componentInstance.addBlock('123, 321');
+    fixture.detectChanges();
+    fixture.componentInstance.addBlock(',,123, 321;');
+    fixture.detectChanges();
+    fixture.componentInstance.addBlock('asd@3gdf.er');
+    fixture.detectChanges();
+    expect(fixture.componentInstance.blocks.length === 2).toBeTruthy();
+  });
+
+  it('should delete block', () => {
+    fixture = TestBed.createComponent(EmailsEditorComponent);
+    const editor = fixture.componentInstance;
+    editor.addBlock('123');
+    fixture.detectChanges();
+    const created = editor.blocks.length === 1;
+    fixture.debugElement.nativeElement.querySelector('email-block').querySelector('.icon-x').click();
+    fixture.detectChanges();
+    expect(created && editor.blocks.length === 0).toBeTruthy();
+  });
+
+  it('should parse pasted blocks', () => {
+    fixture = TestBed.createComponent(EmailsEditorComponent);
+    const editor = fixture.componentInstance;
+    const textarea = fixture.debugElement.nativeElement.querySelector('textarea');
+    textarea.value = '123';
+    $(textarea).trigger('paste');
+    // textarea.dispatchEvent(new Event('paste', { } ));
+    fixture.detectChanges();
+    expect(editor.blocks.length === 1).toBeTruthy();
   });
 });
